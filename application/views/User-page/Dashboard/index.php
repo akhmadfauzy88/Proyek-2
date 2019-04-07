44996077901601cd168953a665ea7b4d6d8153d3
Akhmad Fauzy Epriliawan <ak.fauzy81@gmail.com>
2019-04-07 19:04:34 +0700

    Nyoba push

--- application/views/User-page/Dashboard/index.php
+++ application/views/User-page/Dashboard/index.php
@@ -110,7 +110,7 @@
           </tr>
         </table>
         <hr>
-        PERATURAN PEMINJAMAN
+        PERATURAN PEMINJAMAN RUANGAN
         <br>
         <p>1. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
         ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
