9d3242a43a3d252c995053e96b6aae02c4722361
berlianaputri <42725251+berlianaputri@users.noreply.github.com>
2019-05-01 20:35:04 +0700

    hjhj

--- Procedure.sql
+++ Procedure.sql
@@ -10,18 +10,20 @@ delimiter ;
 delimiter //
 create or replace procedure statuspinjam(uname varchar(32))
 	begin
-	select *
-	from transaksi
+	select * kode_dosen as kdosen
+	from transaksi join dosen on transaksi.kode_dosen = dosen.id
+  join laboratory on transaksi.ruangan = laboratory.id
 	where status = 'Pending' OR status = 'Approved' AND keterangan = 'Kelas' AND peminjam = uname;;
 	end //
 delimiter ;
 
 delimiter //
-create or replace procedure historypinjam(uname varchar(32))
+create or replace procedure history_kelas(id int)
 	begin
-	select *
-	from transaksi
-	where status = 'Attended' OR status = 'Canceled' AND peminjam = uname;
+	select *, kode_dosen as kdosen
+	from transaksi join dosen on transaksi.kode_dosen = dosen.id
+  join laboratory on transaksi.ruangan = laboratory.id
+	where status = 'Attended' OR status = 'Canceled' AND peminjam = id;
 	end //
 delimiter ;
 
@@ -32,5 +34,15 @@ create or replace procedure statuspinjam_praktikum(uname varchar(32))
   from transaksi join dosen on transaksi.kode_dosen = dosen.id
   join laboratory on transaksi.ruangan = laboratory.id
   where status = 'Pending' OR status = 'Approved' AND keterangan = 'Praktikum' AND peminjam = uname;
+  end //
+delimiter ;
+
+delimiter //
+create or replace procedure history_praktikum(id int)
+  begin
+  select *, kode_dosen as kdosen
+  from transaksi join dosen on transaksi.kode_dosen = dosen.id
+  join laboratory on transaksi.ruangan = laboratory.id
+  where status = 'Canceled' OR status = 'Attended' AND keterangan = 'Praktikum' AND peminjam = id;
   end //
 delimiter ;
