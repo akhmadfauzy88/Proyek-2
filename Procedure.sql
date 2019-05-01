439f47c53f2e191e693f92f38e5c4b3f0f8f73d6
berlianaputri <42725251+berlianaputri@users.noreply.github.com>
2019-05-01 20:24:09 +0700

    Procedure stts

--- Procedure.sql
+++ Procedure.sql
@@ -12,7 +12,7 @@ create or replace procedure statuspinjam(uname varchar(32))
 	begin
 	select *
 	from transaksi
-	where status = 'Pending' OR status = 'Approved' AND peminjam = uname;
+	where status = 'Pending' OR status = 'Approved' AND keterangan = 'Kelas' AND peminjam = uname;;
 	end //
 delimiter ;
 
@@ -23,4 +23,14 @@ create or replace procedure historypinjam(uname varchar(32))
 	from transaksi
 	where status = 'Attended' OR status = 'Canceled' AND peminjam = uname;
 	end //
+delimiter ;
+
+delimiter //
+create or replace procedure statuspinjam_praktikum(uname varchar(32))
+  begin
+  select *, kode_dosen as kdosen
+  from transaksi join dosen on transaksi.kode_dosen = dosen.id
+  join laboratory on transaksi.ruangan = laboratory.id
+  where status = 'Pending' OR status = 'Approved' AND keterangan = 'Praktikum' AND peminjam = uname;
+  end //
 delimiter ;
