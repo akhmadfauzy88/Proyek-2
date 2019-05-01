72e069307c848c88d373ecfd23e18041d364cd9a
Akhmad Fauzy Epriliawan <ak.fauzy81@gmail.com>
2019-05-01 20:40:25 +0700

    sqlnyah

--- proyek1.sql
+++ proyek1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 27, 2019 at 04:48 PM
+-- Generation Time: May 01, 2019 at 03:39 PM
 -- Server version: 10.1.29-MariaDB
 -- PHP Version: 7.2.0
 
@@ -21,6 +21,26 @@ SET time_zone = "+00:00";
 --
 -- Database: `proyek1`
 --
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `dashboard` ()  begin
+select * from penjadwalan
+join laboratory on penjadwalan.ruang = laboratory.id
+join kelas on penjadwalan.kelas = kelas.id;
+end$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `statuspinjam_kelas` (IN `uname` VARCHAR(32))  begin
+select *, dosen.kode as kdosen
+from transaksi
+join laboratory on transaksi.ruangan = laboratory.id
+join dosen on transaksi.kode_dosen = dosen.id
+where status = 'Pending' OR status = 'Approved' AND peminjam = uname;
+end$$
+
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -40,6 +60,13 @@ CREATE TABLE `dosen` (
   `lab` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `dosen`
+--
+
+INSERT INTO `dosen` (`id`, `nip`, `kode`, `username`, `password`, `nama_depan`, `nama_belakang`, `email`, `lab`) VALUES
+(1, '4050', 'FZY', 'akfzy', 'cocok', 'Akhmad', 'Fauzy', 'ak@fauzy.com', 3);
+
 -- --------------------------------------------------------
 
 --
@@ -52,6 +79,13 @@ CREATE TABLE `kelas` (
   `dosen_wali` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `kelas`
+--
+
+INSERT INTO `kelas` (`id`, `nama_kelas`, `dosen_wali`) VALUES
+(1, 'D3SI-41-01', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -63,6 +97,22 @@ CREATE TABLE `laboratory` (
   `kode` varchar(5) NOT NULL,
   `nama` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `laboratory`
+--
+
+INSERT INTO `laboratory` (`id`, `kode`, `nama`) VALUES
+(2, 'A1', 'Kelas A1'),
+(3, 'A2', 'Kelas A2'),
+(4, 'B1', 'Database'),
+(5, 'B2', 'Data Mining'),
+(6, 'D3', 'Programming'),
+(7, 'G2', 'Hardware'),
+(8, 'E4', 'Elektronika'),
+(9, 'A7', 'Multimedia'),
+(10, 'D2', 'Software'),
+(11, 'D5', 'Programming');
 
 -- --------------------------------------------------------
 
@@ -93,8 +143,15 @@ CREATE TABLE `mhs` (
   `nama_depan` varchar(20) NOT NULL,
   `nama_belakang` varchar(20) NOT NULL,
   `kelas` int(11) NOT NULL,
-  `email` int(11) NOT NULL
+  `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mhs`
+--
+
+INSERT INTO `mhs` (`id`, `nim`, `username`, `password`, `nama_depan`, `nama_belakang`, `kelas`, `email`) VALUES
+(1, '6701174026', 'berlianaputri', 'cocok', 'Berliana', 'Putri', 1, 'ber@ptr.com');
 
 -- --------------------------------------------------------
 
@@ -109,6 +166,14 @@ CREATE TABLE `penjadwalan` (
   `ruang` int(11) NOT NULL,
   `kelas` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `penjadwalan`
+--
+
+INSERT INTO `penjadwalan` (`id`, `tanggal`, `jam`, `ruang`, `kelas`) VALUES
+(1, '2019-05-01', '09:00:00', 2, 1),
+(2, '2019-05-01', '10:00:00', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -134,7 +199,7 @@ CREATE TABLE `transaksi` (
   `peminjam` int(11) NOT NULL,
   `ruangan` int(11) NOT NULL,
   `jam_masuk` time NOT NULL,
-  `jumlah_jam` time NOT NULL,
+  `jumlah_jam` int(11) NOT NULL,
   `matakuliah` varchar(30) NOT NULL,
   `kode_dosen` int(11) NOT NULL,
   `tanggal` date NOT NULL,
@@ -143,6 +208,13 @@ CREATE TABLE `transaksi` (
   `status` varchar(30) NOT NULL,
   `keterangan` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id`, `peminjam`, `ruangan`, `jam_masuk`, `jumlah_jam`, `matakuliah`, `kode_dosen`, `tanggal`, `kebutuhan`, `bukti`, `status`, `keterangan`) VALUES
+(1, 1, 2, '08:00:00', 3, 'Sql', 1, '2019-05-01', NULL, NULL, 'pending', 'kelas');
 
 --
 -- Indexes for dumped tables
@@ -222,19 +294,19 @@ ALTER TABLE `transaksi`
 -- AUTO_INCREMENT for table `dosen`
 --
 ALTER TABLE `dosen`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `kelas`
 --
 ALTER TABLE `kelas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `laboratory`
 --
 ALTER TABLE `laboratory`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `lak`
@@ -246,13 +318,13 @@ ALTER TABLE `lak`
 -- AUTO_INCREMENT for table `mhs`
 --
 ALTER TABLE `mhs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `penjadwalan`
 --
 ALTER TABLE `penjadwalan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `pesan`
@@ -264,7 +336,7 @@ ALTER TABLE `pesan`
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
@@ -283,16 +355,9 @@ ALTER TABLE `kelas`
   ADD CONSTRAINT `kelas_ibfk_1` FOREIGN KEY (`dosen_wali`) REFERENCES `dosen` (`id`);
 
 --
--- Constraints for table `laboratory`
---
-ALTER TABLE `laboratory`
-  ADD CONSTRAINT `laboratory_ibfk_1` FOREIGN KEY (`id`) REFERENCES `pesan` (`subject`);
-
---
 -- Constraints for table `mhs`
 --
 ALTER TABLE `mhs`
-  ADD CONSTRAINT `mhs_ibfk_1` FOREIGN KEY (`id`) REFERENCES `pesan` (`user`),
   ADD CONSTRAINT `mhs_ibfk_2` FOREIGN KEY (`kelas`) REFERENCES `kelas` (`id`);
 
 --
@@ -301,6 +366,13 @@ ALTER TABLE `mhs`
 ALTER TABLE `penjadwalan`
   ADD CONSTRAINT `penjadwalan_ibfk_1` FOREIGN KEY (`kelas`) REFERENCES `kelas` (`id`),
   ADD CONSTRAINT `penjadwalan_ibfk_2` FOREIGN KEY (`ruang`) REFERENCES `laboratory` (`id`);
+
+--
+-- Constraints for table `pesan`
+--
+ALTER TABLE `pesan`
+  ADD CONSTRAINT `pesan_ibfk_1` FOREIGN KEY (`subject`) REFERENCES `laboratory` (`id`),
+  ADD CONSTRAINT `pesan_ibfk_2` FOREIGN KEY (`user`) REFERENCES `mhs` (`id`);
 
 --
 -- Constraints for table `transaksi`
